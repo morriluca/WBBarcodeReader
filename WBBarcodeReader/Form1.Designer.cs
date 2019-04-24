0e9e641f064a3380934f6acdbde0ba37915a6fbd
morriluca <morriluca@gmail.com>
2019-04-24 14:45:54 +0200

    First Sincy

---
+++ WBBarcodeReader/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace BrApir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(7, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(188, 165);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(7, 9);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(188, 21);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(202, 206);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ WBBarcodeReader/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Video.FFMPEG;
+using System.IO;
+using AForge.Video.VFW;
+using System.Drawing.Imaging;
+
+//using IronBarCode;
+using System.Threading;
+//using z.Barcode;
+using ZXing;
+
+
+
+namespace BrApir
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection VideoCaptureDevices;
+
+        private VideoCaptureDevice FinalVideo = null;
+        private VideoCaptureDeviceForm captureDevice;
+        private Bitmap video;
+        //private AVIWriter AVIwriter = new AVIWriter();
+        private VideoFileWriter FileWriter = new VideoFileWriter();
+        private SaveFileDialog saveAvi;
+        private String lastString = "";
+        private Int32 lastTime = 0;
+        private bool live = true;
+        private Thread mythread = null;
+        public Form1()
+        {
+            this.TopMost = true;
+            InitializeComponent();
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            select_device();
+        }
+
+        void select_device()
+        {
+            VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            int num = 0;
+            foreach (FilterInfo VideoCaptureDevice in VideoCaptureDevices)
+            {
+                comboBox2.Items.Add(VideoCaptureDevice.Name);
+                num++;
+            }
+            if (num > 0)
+            {
+                comboBox2.SelectedIndex = 0;
+              
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+            Environment.Exit(Environment.ExitCode);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void starScann()
+        {
+            FinalVideo = new VideoCaptureDevice(VideoCaptureDevices[comboBox2.SelectedIndex].MonikerString);
+            FinalVideo.VideoResolution = selectResolution(FinalVideo);
+
+            FinalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+            FinalVideo.Start();
+            
+            mythread = new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+
+                while (live)
+                {
+                    check();
+                    Thread.Sleep(100);
+                }
+
+            });
+            mythread.Start();
+        }
+
+        private static VideoCapabilities selectResolution(VideoCaptureDevice device)
+        {
+            foreach (var cap in device.VideoCapabilities)
+            {
+               
+                if (cap.FrameSize.Height == 1080)
+                    return cap;
+                if (cap.FrameSize.Width == 1920)
+                    return cap;
+            }
+            return device.VideoCapabilities.First();
+        }
+
+        void FinalVideo_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            video = (Bitmap)eventArgs.Frame.Clone();
+
+            pictureBox1.Image = video;
+            
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+             
+        }
+
+        public void backgroundchange()
+        {
+            this.BackColor = Color.FromArgb(255, 255, 0);
+            
+           
+        }
+        public void backgroundchange2()
+        {
+            this.BackColor = Color.FromArgb(255, 255, 255);
+         
+        }
+
+        void check(){
+
+            
+            
+            try
+            {
+                Invoke(new MethodInvoker(backgroundchange2));
+
+                IBarcodeReader reader = new BarcodeReader();
+
+                Bitmap cloneBitmap = (Bitmap)video.Clone();
+                var result = reader.Decode(cloneBitmap);      
+                if (result != null)
+                {
+                    bool send = false;
+                    if (lastString.Equals(result.Text))
+                    {
+                        Int32 t = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+                        if (t > lastTime)
+                        {
+                            send = true;
+                        }                       
+                    }else{
+                        send = true;
+                    }
+
+                    if (send)
+                    {
+                        lastString = result.Text;
+                        lastTime = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds + 5;
+                        SendKeys.SendWait(lastString);
+                        SendKeys.SendWait("{ENTER}");
+
+                        Console.WriteLine("GetStarted was a success.  Read Value: " + lastString);
+
+                        System.Media.SoundPlayer player = new System.Media.SoundPlayer(System.IO.Directory.GetCurrentDirectory() + "\\beep-07.wav");
+                        player.Play();
+                        Invoke(new MethodInvoker(backgroundchange));
+                        
+                        
+                    }
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("errore: " + e.ToString());
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (mythread!=null)
+            {
+                mythread.Abort();
+            }
+                
+            starScann();
+        }
+
+     
+      
+    }
+}
